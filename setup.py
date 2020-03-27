b1682b0f8d217fa3fffdacd0a152c3073b307450
Marco de Lannoy Kobayashi <marcodlk@hawaii.edu>
2020-03-27 17:00:28 -0400

    Revert requirements to cookiecutter defaults. Restructure requirements files. Add pyup.yml config file.

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+with open("requirements/install.txt") as requirements_file:
+    requirements = requirements_file.read().split("\n")
 
 setup_requirements = ['pytest-runner', ]
 
